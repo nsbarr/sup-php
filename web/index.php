2f0dd5d5ef4da07ba4959602352803f7c906a7e0
nsbarr <nsbarr@gmail.com>
2016-03-11 14:26:50 -0500

    gottagofast

--- web/index.php
+++ web/index.php
@@ -1,13 +1,3 @@
-<?php require_once('./config.php'); ?>
-
-<form action="charge.php" method="post">
-  <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-          data-key="<?php echo $stripe['publishable_key']; ?>"
-          data-description="Access for a year"
-          data-amount="5000"
-          data-locale="auto"></script>
-</form>
-
 <?php
 
 require('../vendor/autoload.php');
