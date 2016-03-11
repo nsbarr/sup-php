6ab4ece046f685951de770f346c09ef3871c7b6a
nsbarr <nsbarr@gmail.com>
2016-03-11 14:26:21 -0500

    gottagofast

--- web/index.php
+++ web/index.php
@@ -1,6 +1,18 @@
+<?php require_once('./config.php'); ?>
+
+<form action="charge.php" method="post">
+  <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+          data-key="<?php echo $stripe['publishable_key']; ?>"
+          data-description="Access for a year"
+          data-amount="5000"
+          data-locale="auto"></script>
+</form>
+
 <?php
 
 require('../vendor/autoload.php');
+
+
 
 $app = new Silex\Application();
 $app['debug'] = true;
