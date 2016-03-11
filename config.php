3527c89f1fe415d6bbbeb36dfc56339f94e658ca
nsbarr <nsbarr@gmail.com>
2016-03-11 14:23:09 -0500

    gottagofast

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = array(
-  "secret_key"      => "sk_test_4OlzpszvpDJWBNrg7EAl6v3b",
-  "publishable_key" => "pk_test_FoBJucysqa1E5k7n9u1FVhru"
+  "secret_key"      => "sk_live_krEcK3doqljClSJBXd65eRKY",
+  "publishable_key" => "pk_live_aEDQBn9Khn6X7kXNiL5tuMsP"
 );
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
